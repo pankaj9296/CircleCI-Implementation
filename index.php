b0e9b5709fef7cbaf6ba237ce9017b1906e4d0b6
pankaj9296 <pankaj9296@gmail.com>
2017-04-19 15:39:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,16 @@
 $process = curl_init("http://localhost:4040/api/tunnels"); 
 $output = curl_exec($process); 
 curl_close($process); 
-$return = json_decode($output);
-echo $public_url = $return['tunnels'][0]['public_url'];
-echo "<br/><br/><br/>";
+//$return = json_decode($output);
+//echo $public_url = $return['tunnels'][0]['public_url'];
+//echo "<br/><br/><br/>";
 //$return = json_decode($return);
-echo $public_url = $output['tunnels'][0]['public_url'];
 
+echo "complete array :"; print_r($output);
+echo "<br/><br/><br/>";
+echo "without decode :".$public_url = $output['tunnels'][0]['public_url'];
+echo "<br/><br/><br/>";
+$o = json_decode($output);
+echo "with decode array:"; print_r($o);
+echo "<br/><br/><br/>";
+echo "with decode :".$public_url = $o['tunnels'][0]['public_url'];
