34870bd213a265f6e001d6599aeb9d29c9343baa
pankaj9296 <pankaj9296@gmail.com>
2017-04-19 15:55:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <?php
 
-//print_r($_SERVER);
-//print_r($_ENV);
 
-$process = curl_init("http://localhost:4040/api/tunnels"); 
-curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-$output = curl_exec($process); 
-curl_close($process); 
-//$return = json_decode($output);
-//echo $public_url = $return['tunnels'][0]['public_url'];
-//echo "<br/><br/><br/>";
-//$return = json_decode($return);
+$ch = curl_init("http://localhost:4040/api/tunnels");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch); 
+    curl_close($ch); 
+    $tunnels = json_decode($output, true);
+    echo $public_url = $tunnels['tunnels'][0]['public_url'];
+echo "<br/><br/><br/><br/>";
 
-echo "complete array :"; print_r($output);
-echo "<br/><br/><br/>";
-echo "without decode :".$public_url = $output['tunnels'][0]['public_url'];
-echo "<br/><br/><br/>";
-$o = json_decode($output);
-echo "with decode array:"; print_r($o);
-echo "<br/><br/><br/>";
-echo "with decode :".$public_url = $o['tunnels'][0]['public_url'];
+print_r($_SERVER);
+print_r($_ENV);
