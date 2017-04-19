25f4fc181da286e6b40e8f89999792daa38b7256
pankaj9296 <pankaj9296@gmail.com>
2017-04-19 15:18:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
-print_r($_SERVER);
-echo $_SERVER['HTTP_HOST'];
-print_r($_ENV);
+//print_r($_SERVER);
+//print_r($_ENV);
+
+$process = curl_init("http://localhost:4040/api/tunnels"); 
+$return = curl_exec($process); 
+curl_close($process); 
+print_r($return);
 
 
-?>
